119160499cfd4f09c3795dad7d007561b2535bcf
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-02-14 11:38:30 +0000

    Add basic stucture to game code

--- run.py
+++ run.py
@@ -1,3 +1,59 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+
+class Board:
+    """
+    Main board class. which contain and sets board size the number of ships
+     the players name and board type(player board or computer)
+     Has methods for adding ships and gueses and printing the board
+    """
+
+    def __init__(selfm sizem num_shipsm namem type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+
+
+    def guess(self, x, y):
+
+
+    def add_ship():
+
+
+    def random_point():
+
+    
+    def valid_corordinates():
+
+
+    def populate_board():
+
+
+    def make_guess():
+
+
+    def play_game():
+
+
+    def new_game():
+
+
+    new_game()
+
+
+
+    
+
+
+    
+
+
+
